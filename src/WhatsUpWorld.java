6392e796c735e18eab1d38f9b9be7d1f8df8fbaa
Walter Smith <walter.c.smith01@gmail.com>
2023-04-17 10:48:04 -0500

    Getting back familiar with the system out feature to printing out to the console

---
+++ src/WhatsUpWorld.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+public class WhatsUpWorld {
+    public static void main(String[] args){
+
+        String worldDeclaration = "What's Up World!!\nI \'m Back!!";
+        //This prints out What's up World! I'm back!
+        System.out.println(worldDeclaration);
+
+        // Declaring/initializing a variable
+
+        float myFavNumber = (float) 3.14;
+
+        // Printing out myFavNumber variable to the console
+
+        System.out.println(myFavNumber);
+//
+//        //Declaring/initializing a string variable and printing out to the console
+//
+        String nickName = "Wally";
+        System.out.println(nickName);
+//
+//
+        String myString = "Great";
+        char oneCharacter = 'A';
+
+        System.out.println(myString);
+
+        System.out.println(oneCharacter);
+
+//      Observing the power of post and pre increment
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+//        int x2 = 5;
+//        System.out.println(++x2);
+//        System.out.println(x2);
+//
+//        int x = 4;
+//        int longHandAdding = x = x + 5;
+//
+//        int y = 4;
+//        int shortHandAdding = y += 5;
+//
+//
+//        System.out.println(longHandAdding);
+//        System.out.println(shortHandAdding);
+
+//        int a = 3;
+//        int b = 4;
+//        int longHand;
+//        longHand = b = b * a;
+
+
+//        //b *= a;
+//        int c = 3;
+//        int d = 4;
+//        int shortHand = c *= d;
+
+//        int e = 10;
+//        int f = 2;
+////         e /= f;
+//         f -= e;
+//
+//        System.out.println(f);
+
+//        int longHand = e = e / y ;
+//
+//        int shortHand = e /= y;
+//
+//
+//        System.out.println(longHand);
+//        System.out.println(shortHand);
+
+//        byte smallContainer = 127;
+//        ++smallContainer;
+//
+//        System.out.println(smallContainer);
+//
+//        // Printing to the console and using placeholders "%s"
+//        String name = "Walt";
+//        System.out.printf("I use the name %s, instead of Walter \n", name);
+
+        //Getting inputs to the console
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter something: ");
+//        String userInput = scanner.nextLine();
+//
+//        System.out.println("You entered: --> \"" + userInput + "\" <-- " );
+
+        double pi = 3.14159;
+        double someNumber = 4.873;
+
+        System.out.println(pi);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the variable called pi: ");
+        Float userInput = sc.nextFloat();
+        System.out.println("You entered: pi is equal to " + userInput);
+
+//        Scanner sc = new Scanner(System.in);
+//        int i = sc.nextInt();
+//        System.out.println("Input Something here: ");
+
+//        String userInput2 = sc.nextLine();
+//        System.out.println(pi);
+//        System.out.format;
+
+//        Scanner anotherScanner = new Scanner(System.in);
+//        System.out.println("What is the value of pi?");
+//        String userInput2 = scanner.nextLine();
+//
+//        System.out.println("The value of pi is approximately 3.14.");
+
+
+
+
+
+    }
+}
