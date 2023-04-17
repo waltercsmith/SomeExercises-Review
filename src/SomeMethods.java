e5978ce5e9e57cdda989bc544939ccd69f1ba671
Walter Smith <walter.c.smith01@gmail.com>
2023-04-17 18:28:27 -0500

    Created a separate class and methods both void and return types

--- src/SomeMethods.java
+++ src/SomeMethods.java
@@ -1,17 +1,6 @@
 
-class User{
-    public void createPost(){
-        System.out.println("Created New Post");
-    }
 
-    public void deletePost(){
-        System.out.println("Deleted Post");
-    }
 
-    public void updatePost(){
-        System.out.println("Updated Post");
-    }
-}
     public class SomeMethods {
 
         public static String sayHey(String input){
@@ -22,27 +11,27 @@ class User{
             return "Yo, " + input2;
         }
 
-        public static String drinkBeverage(String input3){
-            return "Drinking beverage called: " + input3;
+        public static void changeString(String s) {
+            s = "Wub a lub a dub dub";
         }
 
         public static void main(String[] args){
 
 
-            sayHey("Walter");
-            String sayHeyWalter = sayHey("Walter");
+//            sayHey("Walter");
+//            String sayHeyWalter = sayHey("Walter"); // ---> called/invoked sayHey method
+//
+//            System.out.println(sayHey("Walter"));
+//
+//
+//            System.out.println(sayHeyWalter);
+//
+//            System.out.println(sayYo(5));
 
-            System.out.println(sayHey("Walter"));
 
 
-            System.out.println(sayHeyWalter);
 
-            System.out.println(sayYo(5));
 
-            System.out.println(drinkBeverage("Coffee"));
-
-            User newPerson = new User();
-            newPerson.createPost();
 
 
 
---
+++ src/User.java
@@ -0,0 +1,22 @@
+public class User {
+    //In the future create User class in another page (completed)
+
+        public String drinkBeverage(String beverage){
+            return "Drinking beverage called: " + beverage;
+        }
+
+
+
+        public void createPost(){
+            System.out.println("Created New Post");
+        }
+
+        public void deletePost(){
+            System.out.println("Deleted Post");
+        }
+
+        public void updatePost(){
+            System.out.println("Updated Post");
+        }
+
+}
